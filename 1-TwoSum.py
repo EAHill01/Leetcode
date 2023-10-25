46fbbecc708b0c027685a42bb1702ef4b6cfb82a
Erica <106079862+EAHill01@users.noreply.github.com>
2023-10-25 21:19:43 +0100

    Update 1-TwoSum.py

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -1,3 +1,13 @@
+#works, second edit, ~94% run and ~41% mem
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for a, b in enumerate(nums):
+            d[b] = a
+        for a, b in enumerate(nums):
+            if ((target-b) in d) and (d[target-b] != a):
+                return (a,d[target-b])
+
 #works, ~47% run and ~21% mem
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
